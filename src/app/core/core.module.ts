535c4962ffefff87df910b556e67c03a0ace412a
Mavriche Raluca <ralucamavriche@gmail.com>
2021-03-11 19:58:06 +0200

    Creating the Core Module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([])
+  ],
+  declarations: [
+    BsNavbarComponent,
+    HomeComponent,
+    LoginComponent,
+  ],
+  exports: [
+    BsNavbarComponent
+  ]
+})
+export class CoreModule { }
